33fc54b14ed12166c0770aa276ad1cbcdd47425d
adarshhlohar <adarshhlohar@gmail.com>
2023-11-28 21:08:29 +0530

    Ability to edit existing contact person using their name

--- AddContact.java
+++ AddContact.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddContact {
     
+    ArrayList<ContactDetail> list = new ArrayList<>();
     //Use Case-2 Ability to add a new Contact to Address Book
+    Scanner sc = new Scanner(System.in);
     void addContact() {
             ContactDetail contact = new ContactDetail();
-            Scanner sc = new Scanner(System.in);
-
             System.out.println("Enter the first name:");
             contact.setFirstName(sc.next());
             System.out.println("Enter the last name:");
@@ -23,6 +24,47 @@ public class AddContact {
             contact.setPhoneNumber(sc.next());
             System.out.println("Enter the email_id:");
             contact.setEmailId(sc.next());
-            sc.close();
+            list.add(contact);
+}
+
+    void display(){
+            System.out.println(list.toString());
+    }
+
+//Use Case3: method for editing the existing contact
+void editContact() {
+    boolean isPersonFound=false;
+    int indexOfPerson=0;
+    System.out.println("Enter name for editing an existing contact:");
+    String firstName = sc.next();
+    for(int i=0; i<list.size(); i++) {
+        if(list.get(i).getFirstName().equals(firstName)) {
+            isPersonFound=true;
+            indexOfPerson=i;
+            break;
+        }
+    }
+    if(isPersonFound) {
+        System.out.println("Update the first name:");
+        list.get(indexOfPerson).setFirstName(sc.next());
+        System.out.println("Update Last Name: ");
+        list.get(indexOfPerson).setLastName(sc.next());
+        System.out.println("Update address: ");
+        list.get(indexOfPerson).setAddress(sc.next());
+        System.out.println("Update city: ");
+        list.get(indexOfPerson).setCity(sc.next());
+        System.out.println("Update state: ");
+        list.get(indexOfPerson).setState(sc.next());
+        System.out.println("Update zip code: ");
+        list.get(indexOfPerson).setZipCode(sc.next());
+        System.out.println("Update phone number: ");
+        list.get(indexOfPerson).setPhoneNumber(sc.next());
+        System.out.println("Update email_id: ");
+        list.get(indexOfPerson).setEmailId(sc.next());
+    }
+    else {
+        System.out.println("Sorry,name does not exist");
+    }
+    sc.close();
 }
 }
--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class AddressBook{
     public static void main(String[] args) {
         System.out.println("Hii and Welcome to MY ADDRESS BOOK Project !!!");
         AddContact add = new AddContact();
-        add.addContact();
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Menu Enter 1 for add Contact \nEnter 2 for edit contact");
+            int input = sc.nextInt();
+            switch (input) {
+                case 1:
+                    add.addContact();
+                    add.display();
+                    break;
+            
+
+                case 2:
+                    add.editContact();
+                    add.display();
+
+                case 3:
+                    System.out.println("Exiting the program.");
+                    System.exit(0);
+                default:
+                    System.out.println("Youre Entered Wrong Input Please input right input");
+                    break;
+            }
+        }while(true);
     }
 }
